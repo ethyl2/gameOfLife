8c414922bbd50322c0d7eea9c07c05f81a0d435a
Heather Nuffer <zzzuzz@gmail.com>
2020-07-04 21:50:31 -0600

    Adds custom header image and favicon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Game from './Game';
 import './App.css';
+import GOLBanner2 from './images/GOLBanner2.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Conway's Game of Life</header>
+      <header className="App-header">
+        <img src={GOLBanner2} alt="Conway's Game of Life" />
+      </header>
       <main>
         <Game />
       </main>
