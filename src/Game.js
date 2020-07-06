c45bdf4b874bf49a5f40c77b9329a646a751c7bf
Heather Nuffer <zzzuzz@gmail.com>
2020-07-05 19:17:24 -0600

    Implements basic calculateNeighbors() function

--- src/Game.js
+++ src/Game.js
@@ -156,6 +156,53 @@ function Game() {
     setBoard(makeEmptyBoard());
   };
 
+  function calculateNeighbors(currentX, currentY) {
+    let totalAliveNeighbors = 0;
+    const neighborBooleans = [];
+    // Top row:
+    for (let i = -1; i < 2; i++) {
+      // console.log(`Checking board[${currentY - 1}][${currentX + i}]`);
+      if (board[currentY - 1][currentX + i]) {
+        totalAliveNeighbors++;
+        neighborBooleans.push(true);
+      } else {
+        neighborBooleans.push(false);
+      }
+    }
+
+    // Middle row:
+    if (board[currentY][currentX - 1]) {
+      totalAliveNeighbors++;
+      neighborBooleans.push(true);
+    } else {
+      neighborBooleans.push(false);
+    }
+
+    if (board[currentY][currentX + 1]) {
+      totalAliveNeighbors++;
+      neighborBooleans.push(true);
+    } else {
+      neighborBooleans.push(false);
+    }
+
+    // Bottom row:
+    for (let i = -1; i < 2; i++) {
+      if (board[currentY + 1][currentX + i]) {
+        totalAliveNeighbors++;
+        neighborBooleans.push(true);
+      } else {
+        neighborBooleans.push(false);
+      }
+    }
+
+    console.log(totalAliveNeighbors);
+    console.log(neighborBooleans);
+  }
+
+  useEffect(() => {
+    calculateNeighbors(1, 1);
+  }, [numClicks]);
+
   return (
     <div className="Game">
       <div>
