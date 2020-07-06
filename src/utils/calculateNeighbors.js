11ffeaf07fc98f77d4d6219f8ec051eb474fba0c
Heather Nuffer <zzzuzz@gmail.com>
2020-07-05 20:05:26 -0600

    Adds special case in calculateNeighbors() when x & y === 0

--- src/utils/calculateNeighbors.js
+++ src/utils/calculateNeighbors.js
@@ -18,20 +18,16 @@ export default function calculateNeighbors(
     [currentX + 1, currentY + 1],
   ];
 
-  for (let i = 0; i < neighbors.length; i++) {
-    const xToCheck = neighbors[i][0];
-    const yToCheck = neighbors[i][1];
-    if (board[yToCheck][xToCheck]) {
-      totalAliveNeighbors++;
-      neighborBooleans.push(true);
-    } else {
-      neighborBooleans.push(false);
-    }
-  }
+  // Modify neighbors for special edge cases:
 
-  // Special edge cases:
   // When currentX === 0 && currentY === 0:
-
+  if (currentX === 0 && currentY === 0) {
+    neighbors[0] = [cols - 1, rows - 1];
+    neighbors[1][1] = rows - 1;
+    neighbors[2][1] = rows - 1;
+    neighbors[3][0] = cols - 1;
+    neighbors[5][0] = cols - 1;
+  }
   // When currentX === 0 && currentY = rows-1
 
   // When currentX === cols-1 && currentY = rows-1
@@ -45,6 +41,17 @@ export default function calculateNeighbors(
   // When y === 0
 
   // When y === rows-1
+
+  for (let i = 0; i < neighbors.length; i++) {
+    const xToCheck = neighbors[i][0];
+    const yToCheck = neighbors[i][1];
+    if (board[yToCheck][xToCheck]) {
+      totalAliveNeighbors++;
+      neighborBooleans.push(true);
+    } else {
+      neighborBooleans.push(false);
+    }
+  }
 
   console.log(totalAliveNeighbors);
   console.log(neighborBooleans);
